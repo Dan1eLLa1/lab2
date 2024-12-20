7b547cbc1665281c75209228d475558cfce27797
Dan1eLLa1 <ilsiyar_salahova@mail.ru>
2024-12-21 03:21:34 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Lab2_Wizhener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_EncryptText = new System.Windows.Forms.TextBox();
+            this.textBox_DecryptText = new System.Windows.Forms.TextBox();
+            this.textBox_Key = new System.Windows.Forms.TextBox();
+            this.button_Encrypt = new System.Windows.Forms.Button();
+            this.button_Decrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_EncryptText
+            // 
+            this.textBox_EncryptText.Location = new System.Drawing.Point(365, 50);
+            this.textBox_EncryptText.Multiline = true;
+            this.textBox_EncryptText.Name = "textBox_EncryptText";
+            this.textBox_EncryptText.Size = new System.Drawing.Size(169, 214);
+            this.textBox_EncryptText.TabIndex = 0;
+            this.textBox_EncryptText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress_CryptTextBox);
+            // 
+            // textBox_DecryptText
+            // 
+            this.textBox_DecryptText.Location = new System.Drawing.Point(12, 50);
+            this.textBox_DecryptText.Multiline = true;
+            this.textBox_DecryptText.Name = "textBox_DecryptText";
+            this.textBox_DecryptText.Size = new System.Drawing.Size(169, 214);
+            this.textBox_DecryptText.TabIndex = 1;
+            this.textBox_DecryptText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress_CryptTextBox);
+            // 
+            // textBox_Key
+            // 
+            this.textBox_Key.Location = new System.Drawing.Point(227, 206);
+            this.textBox_Key.Name = "textBox_Key";
+            this.textBox_Key.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Key.TabIndex = 2;
+            this.textBox_Key.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress_CryptTextBox);
+            // 
+            // button_Encrypt
+            // 
+            this.button_Encrypt.Location = new System.Drawing.Point(12, 280);
+            this.button_Encrypt.Name = "button_Encrypt";
+            this.button_Encrypt.Size = new System.Drawing.Size(169, 23);
+            this.button_Encrypt.TabIndex = 3;
+            this.button_Encrypt.Text = "Зашифровать";
+            this.button_Encrypt.UseVisualStyleBackColor = true;
+            this.button_Encrypt.Click += new System.EventHandler(this.Encode);
+            // 
+            // button_Decrypt
+            // 
+            this.button_Decrypt.Location = new System.Drawing.Point(365, 280);
+            this.button_Decrypt.Name = "button_Decrypt";
+            this.button_Decrypt.Size = new System.Drawing.Size(169, 23);
+            this.button_Decrypt.TabIndex = 4;
+            this.button_Decrypt.Text = "Расшифровать";
+            this.button_Decrypt.UseVisualStyleBackColor = true;
+            this.button_Decrypt.Click += new System.EventHandler(this.Decode);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Открытый текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Закрытый текст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "КЛЮЧ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Decrypt);
+            this.Controls.Add(this.button_Encrypt);
+            this.Controls.Add(this.textBox_Key);
+            this.Controls.Add(this.textBox_DecryptText);
+            this.Controls.Add(this.textBox_EncryptText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_EncryptText;
+        private System.Windows.Forms.TextBox textBox_DecryptText;
+        private System.Windows.Forms.TextBox textBox_Key;
+        private System.Windows.Forms.Button button_Encrypt;
+        private System.Windows.Forms.Button button_Decrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+
+namespace Lab2_Wizhener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя ";
+        private void KeyPress_CryptTextBox(object sender, KeyPressEventArgs e)
+        {
+
+            if (!(e.KeyChar >= 1040 && e.KeyChar <= 1103) && e.KeyChar != 8 && e.KeyChar != 32 && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+        private void Encode(object sender, EventArgs e)
+        {
+            int keyindex = 0;
+            if (textBox_DecryptText.Text.Length != 0 && textBox_Key.Text.Length != 0)
+                foreach (char symbol in textBox_DecryptText.Text)
+                {
+                    textBox_EncryptText.Text += alphabet[(alphabet.IndexOf(symbol) + alphabet.IndexOf(textBox_Key.Text[keyindex])) % alphabet.Length];
+                    keyindex = (keyindex + 1) % textBox_Key.Text.Length;
+                }
+            else MessageBox.Show("Ошибка!",null,MessageBoxButtons.OK);
+        }
+        private void Decode(object sender, EventArgs e)
+        {
+            int keyindex = 0;
+            if (textBox_EncryptText.Text.Length != 0 && textBox_Key.Text.Length != 0)
+                foreach (char symbol in textBox_EncryptText.Text)
+                {
+                    textBox_DecryptText.Text += alphabet[(alphabet.IndexOf(symbol) +alphabet.Length- alphabet.IndexOf(textBox_Key.Text[keyindex])) % alphabet.Length];
+                    keyindex = (keyindex + 1) % textBox_Key.Text.Length;
+                }
+            else MessageBox.Show("Ошибка!", null, MessageBoxButtons.OK);
+        }
+        void Open()
+        {
+            string x = textBox_EncryptText.Text; 
+            int maxKeyLength = 10;
+            double [] indexOfCoincidence = new double[maxKeyLength];
+            // Вычисляем индексы совпадения для разных длин ключа
+            for (int k = 0; k < maxKeyLength; k++)
+            {
+                indexOfCoincidence[k] = CalculateIndexOfCoincidenceForKeyLength(x, k+1);
+                Console.WriteLine($"Длина ключа {k+1}: Индекс совпадения = {indexOfCoincidence[k]:F4}");
+            }
+        
+
+            double CalculateIndexOfCoincidenceForKeyLength(string text, int keyLength)
+            {
+                // Группы для символов (по остатку от деления на длину ключа)
+                List<string> groups = new List<string>();
+
+                // Инициализируем группы
+                for (int i = 0; i < keyLength; i++)
+                {
+                    groups.Add("");
+                }
+
+                // Разбиваем строку на группы
+                for (int i = 0; i < text.Length; i++)
+                {
+                    groups[i % keyLength] += text[i];
+                }
+
+                // Вычисляем средний индекс совпадения по всем группам
+                double totalIndexOfCoincidence = 0;
+
+                foreach (var group in groups)
+                {
+                     totalIndexOfCoincidence += CalculateIndexOfCoincidence(group);
+                }
+
+                 return totalIndexOfCoincidence / keyLength;
+            }
+
+            double CalculateIndexOfCoincidence(string text)
+            {
+                int n = text.Length;
+                if (n < 2) return 0;
+
+                // Подсчет частот символов
+                Dictionary<char, int> frequency = new Dictionary<char, int>();
+                foreach (var c in text)
+                {
+                    if (frequency.ContainsKey(c))
+                        frequency[c]++;
+                    else
+                        frequency[c] = 1;
+                }
+
+                // Вычисляем индекс совпадения
+                double index = 0;
+                foreach (var freq in frequency.Values)
+                {
+                    index += freq * (freq - 1);
+                }
+
+                return index / (n * (n - 1));
+            }
+            int keylenght = Array.IndexOf(indexOfCoincidence, indexOfCoincidence.Max());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
+
+    }   
+}
+
+
+   
